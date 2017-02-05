442d73ce55a9a536ed347ffb22100b22b91dda1b
Артем <insidia93@gmail.com>
2017-02-05 16:21:36 +0200

    В наш класс новости мы добавили поля название ,текст и дата,  а так же передаем в наш метод название класса

--- model/News.php
+++ model/News.php
@@ -2,11 +2,15 @@
 require_once  __DIR__ . '/../classes/DB.php';
 class News
 {
+    public $title;
+    public $text;
+    public $date;
+
 
     public static function  getALL(){
         $db = new DB();
         $sql = "SELECT * FROM news";
-        return $db->getAll($sql);
+        return $db->getAll($sql,'News');
 
     }
 }
