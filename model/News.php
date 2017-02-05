3373616c00ca54fc449470d3cf0ceed112a64e1d
Артем <insidia93@gmail.com>
2017-02-05 15:38:29 +0200

    Изменили нашу функию и сделали ее статик, для того чтобы вызывать ее без создания обьекта News

--- model/News.php
+++ model/News.php
@@ -3,7 +3,7 @@ require_once  __DIR__ . '/../classes/DB.php';
 class News
 {
 
-    public function  getALL(){
+    public static function  getALL(){
         $db = new DB();
         $sql = "SELECT * FROM news";
         return $db->getAll($sql);
